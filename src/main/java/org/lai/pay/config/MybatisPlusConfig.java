8ba71fc118b698fdb00fe0f5b39327a4fad1af60
laibinglin <laibinglin@git.com>
2024-08-31 08:04:52 +0800

    init

---
+++ src/main/java/org/lai/pay/config/MybatisPlusConfig.java
@@ -0,0 +1,7 @@
+package org.lai.pay.config;/**
+*@Author: zurichscud
+*@Date: 2024/8/30 下午11:41
+*@Description: TODO
+*/
+public class MybatisPlusConfig {
+}
---
+++ src/main/java/org/lai/pay/controller/TestController.java
@@ -0,0 +1,7 @@
+package org.lai.pay.controller;/**
+*@Author: zurichscud
+*@Date: 2024/8/31 上午12:58
+*@Description: TODO
+*/
+public class TestController {
+}
